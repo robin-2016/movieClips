28cd2a8fe6c67132da8d46d21432779f5d60f128
Robin <xuhongbin3241@gmail.com>
2018-05-06 21:37:29 +0800

    mod photo and test video

--- pages/index/index.js
+++ pages/index/index.js
@@ -11,9 +11,9 @@ Page({
       { text: "《当幸福来敲门》片段", url: "https://vide-test-1255927624.cos.ap-beijing.myqcloud.com/dxflqm.jpg" }
     ],
     images: [
-      { text: "《急速追杀》片段", url: "https://vide-test-1255927624.cos.ap-beijing.myqcloud.com/jszs.jpg" },
-      { text: "《琅琊榜》片段", url: "https://vide-test-1255927624.cos.ap-beijing.myqcloud.com/lyb.png" },
-      { text: "《杀手莱昂》片段", url: "https://vide-test-1255927624.cos.ap-beijing.myqcloud.com/ssla.png" }
+      { text: "《急速追杀》片段", url: "https://vide-test-1255927624.cos.ap-beijing.myqcloud.com/jszs01.jpg" },
+      { text: "《琅琊榜》片段", url: "https://vide-test-1255927624.cos.ap-beijing.myqcloud.com/lyb01.png" },
+      { text: "《杀手莱昂》片段", url: "https://vide-test-1255927624.cos.ap-beijing.myqcloud.com/ssla01.png" }
     ]
   },
   //事件处理函数
